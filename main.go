6faa6c3c234e2bbb48317d358b2efc25b301c082
Shamar Droghetti <droghetti@motorialab.com>
2015-06-25 16:07:58 +0200

    first commit

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+    "fmt"
+	"time"
+    "net/http"
+)
+
+
+func _unixtime(w http.ResponseWriter, r *http.Request) {
+    t := time.Now()
+	fmt.Fprintf(w, "%du", t.UTC().Unix())
+}
+
+func _utctime(w http.ResponseWriter, r *http.Request) {
+    t := time.Now()
+	fmt.Fprintf(w,"%s", t.UTC())
+}
+
+func main() {
+    http.HandleFunc("/", _unixtime)
+    http.HandleFunc("/utctime", _utctime)
+    http.ListenAndServe(":8080", nil)
+}
